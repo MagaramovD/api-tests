6f79c4f55b73b0e0f4d412914e3976dd0a7b0ceb
Din Magaramov <din.magaramov@natlink.fi>
2025-06-21 17:57:52 +0300

    fix decorators

--- project/tests/test_ui.py
+++ project/tests/test_ui.py
@@ -5,10 +5,12 @@ import allure
 
 # Playwr
 
-@allure.dynamic.title("Successful login with valid credentials")
-@allure.dynamic.severity(allure.severity_level.CRITICAL)
-@allure.dynamic.description("This test verifies that a user can log in using correct credentials.")
+
 def test_login(page: Page, saucedemo_url):
+    allure.dynamic.title("Successful login with valid credentials")
+    allure.dynamic.severity(allure.severity_level.CRITICAL)
+    allure.dynamic.description(
+        "This test verifies that a user can log in using correct credentials.")
     allure.dynamic.tag("UI", "Login")
     with allure.step("Enter to website"):
         page.goto(saucedemo_url)
